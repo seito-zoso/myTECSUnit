22405511d50fbaedb56cbd26682c45b67ee6f3c5
shirata-seito <ioplkj0987@gmail.com>
2019-06-14 16:48:38 +0900

    [append] target_struct.h [modify] tTarget3.c, target.cdl
    構造体のテスト対象を作成、（ひとまずin指定で）

--- src/tTarget3.c
+++ src/tTarget3.c
@@ -74,6 +74,28 @@ eTarget3_sendMessage(CELLIDX idx, const char_t* buf, int8_t len)
 	return 1;
 }
 
+/* #[<ENTRY_FUNC>]# eTarget3_sendStruct
+ * name:         eTarget3_sendStruct
+ * global_name:  tTarget3_eTarget3_sendStruct
+ * oneway:       false
+ * #[</ENTRY_FUNC>]# */
+int
+eTarget3_sendStruct(CELLIDX idx, const struct target_struct* data)
+{
+	CELLCB	*p_cellcb;
+	if (VALID_IDX(idx)) {
+		p_cellcb = GET_CELLCB(idx);
+	}
+	else {
+		/* エラー処理コードをここに記述します */
+	} /* end if VALID_IDX(idx) */
+
+	/* ここに処理本体を記述します #_TEFB_# */
+	printf("This is eTarget3_sendStruct\n");
+	printf("num = %d\n", data->num );
+	printf("str = %s\n", data->str );
+}
+
 /* #[<ENTRY_FUNC>]# eTarget3_receiveMessage
  * name:         eTarget3_receiveMessage
  * global_name:  tTarget3_eTarget3_receiveMessage
